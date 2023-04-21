77e18cda78382ce136480e761c4db33bfe5f4b8e
nicolycarvalh <nicolycarvalh15@gmail.com>
2023-04-21 20:34:44 -0300

    Validação dos campos vazios na tela de login e cadastro

--- app/src/main/java/com/example/appmultiplastelas/Cadastro.java
+++ app/src/main/java/com/example/appmultiplastelas/Cadastro.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Cadastro extends AppCompatActivity  {
 
     Button btVoltar, btSalvar;
+
+    TextView etNome, etUsuario, etSenha, etDataNascimento, cbMasculino, cbFeminino, etTelefone;
+
+    String nome, usuario, senha, nascimento, masc, fem, tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,24 @@ public class Cadastro extends AppCompatActivity  {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = etUsuario.getText().toString().trim();
+                senha = etSenha.getText().toString().trim();
+                nome = etNome.getText().toString().trim();
+                nascimento = etDataNascimento.getText().toString().trim();
+                masc = cbMasculino.getText().toString().trim();
+                fem = cbFeminino.getText().toString().trim();
+                tel = etTelefone.getText().toString().trim();
+                if (usuario.isEmpty() || senha.isEmpty() || nome.isEmpty() || nascimento.isEmpty() || masc.isEmpty() || fem.isEmpty() || tel.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Cadastro Inválido. Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Cadastro Efetuado!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
