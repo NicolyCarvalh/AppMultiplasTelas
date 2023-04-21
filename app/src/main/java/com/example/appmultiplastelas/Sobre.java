8c446876dbfd0b1fc72dee6d0e67d69bf1e30159
Nathalia S. Amaral <nathaliasantosamaral02@gmail.com>
2023-04-21 19:59:18 -0400

    Adicionando site da UFMS ao respectivo botão.

--- app/src/main/java/com/example/appmultiplastelas/Sobre.java
+++ app/src/main/java/com/example/appmultiplastelas/Sobre.java
@@ -3,25 +3,36 @@ package com.example.appmultiplastelas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Sobre extends AppCompatActivity {
 
-    Button btVoltar;
+    Button btVoltar, btUf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sobre);
 
         btVoltar = (Button) findViewById(R.id.btVoltar);
+        btUf = findViewById(R.id.btUf);
 
         btVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btUf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://cpan.ufms.br/");
+                Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(it);
             }
         });
     }
