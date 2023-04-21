74fecfe969828dd4eab1eff2cba1729f12650cb8
nicolycarvalh <nicolycarvalh15@gmail.com>
2023-04-21 19:16:29 -0300

    Arrumando layout de todas as telas

--- app/src/main/java/com/example/appmultiplastelas/Login.java
+++ app/src/main/java/com/example/appmultiplastelas/Login.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btVoltar, btSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        btVoltar = (Button) findViewById(R.id.btVoltar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void telaHome(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+
 }
--- app/src/main/java/com/example/appmultiplastelas/Sobre.java
+++ app/src/main/java/com/example/appmultiplastelas/Sobre.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Sobre extends AppCompatActivity {
 
+    Button btVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sobre);
-    }
 
-    public void telaHome(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        btVoltar = (Button) findViewById(R.id.btVoltar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
