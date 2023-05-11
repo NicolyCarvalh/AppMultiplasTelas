2f88ee54e63f373ea4d59ac6dfd40f3d96a4d65e
nicolycarvalh <nicolycarvalh15@gmail.com>
2023-05-11 20:26:47 -0300

    Adicionando Logs

--- app/src/main/java/com/example/appmultiplastelas/MainActivity.java
+++ app/src/main/java/com/example/appmultiplastelas/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i("INFORMATION","ON START");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i("INFORMATION","ON STOP");
     }
 }
 
